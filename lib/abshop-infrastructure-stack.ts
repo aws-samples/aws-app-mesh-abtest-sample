f96730e3c5d171d78f8f9d4310484186c7efc92c
Vincent Lesierse <vinles@amazon.nl>
2024-05-03 22:21:40 +0200

    Update and streamline dependencies

--- lib/abshop-infrastructure-stack.ts
+++ lib/abshop-infrastructure-stack.ts
@@ -15,29 +15,34 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-import { Construct } from 'constructs';
-import * as cdk from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as eks from 'aws-cdk-lib/aws-eks';
-import { AppMesh } from './AppMesh';
-import { CloudWatchAgent } from './CloudWatchAgent';
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as eks from "aws-cdk-lib/aws-eks";
+import { AppMesh } from "./AppMesh";
+import { CloudWatchAgent } from "./CloudWatchAgent";
 
 export class ABShopInfrastructureStack extends cdk.Stack {
-
   public cluster: eks.Cluster;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     var vpc = new ec2.Vpc(this, "VPC");
-    this.cluster = new eks.Cluster(this, "Cluster", { vpc, version: eks.KubernetesVersion.V1_20, defaultCapacity: 3 });
+    this.cluster = new eks.Cluster(this, "Cluster", {
+      vpc,
+      version: eks.KubernetesVersion.V1_28,
+      defaultCapacity: 3,
+    });
 
-    new cdk.CfnOutput(this, 'UpdateKubeConfig', { value: `aws eks update-kubeconfig --name ${this.cluster.clusterName} --region ${this.region} --role-arn ${this.cluster.kubectlRole?.roleArn}` });
+    new cdk.CfnOutput(this, "UpdateKubeConfig", {
+      value: `aws eks update-kubeconfig --name ${this.cluster.clusterName} --region ${this.region} --role-arn ${this.cluster.kubectlRole?.roleArn}`,
+    });
 
     // Container Insights
-    new CloudWatchAgent(this, 'CloudWatchAgent', { cluster: this.cluster} );
+    new CloudWatchAgent(this, "CloudWatchAgent", { cluster: this.cluster });
 
     // App Mesh
-    new AppMesh(this, 'AppMesh', { cluster: this.cluster });
+    new AppMesh(this, "AppMesh", { cluster: this.cluster });
   }
 }
